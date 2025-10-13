a5ae424daffc7449364a25c94afc256d07423b9f
SombreMael <sombremael@gmail.com>
2025-10-13 12:28:34 +0100

    build v2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'CONSTELLATION';
+const isProduction = process.env.NODE_ENV === true ;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: isProduction ? `/${repoName}/` : '/',
   build: {
     outDir: 'dist',
   },
